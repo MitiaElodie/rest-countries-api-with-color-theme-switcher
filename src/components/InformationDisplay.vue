30f631e2150f2b8b57401e773020f2bef2d206eb
Anjara Mitia Elodie <elodie.mitia@conformit.com>
2023-06-20 17:54:17 -0400

    HTML + JS: Create component InformationDisplay and InformationList

---
+++ src/components/InformationDisplay.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+   name: 'InformationDisplay',
+   props: {
+      information: {
+         type: Object,
+         default: () => ({
+            name: '',
+            value: '',
+         })
+      }
+   }
+}
+</script>
+
+<template>
+<div class="information-display">
+   <span class="information-display__label">{{ information.name + ':' }}</span>
+   <span class="information-display__value">{{ information.value }}</span>
+</div>
+</template>
---
+++ src/components/InformationList.vue
@@ -0,0 +1,24 @@
+<script>
+import InformationDisplay from '@/components/InformationDisplay.vue';
+
+export default {
+   name: 'InformationList',
+   components: { InformationDisplay },
+   props: {
+      informationList: {
+         type: Array,
+         default: () => []
+      }
+   },
+}
+</script>
+
+<template>
+<div class="information-list">
+   <InformationDisplay
+      v-for="(item, index) in informationList"
+      :information="item"
+      :key="`informationDisplay${ index }`"
+   />
+</div>
+</template>
