8049c812be5524020bca0c3fdf7c6df12913d9ae
Anjara Mitia Elodie <elodie.mitia@conformit.com>
2023-06-20 17:59:22 -0400

    CSS: remove unneeded css

--- src/App.vue
+++ src/App.vue
@@ -10,18 +10,9 @@ export default {
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <CountryCard msg="Welcome to Your Vue.js App"/>
 </template>
 
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
