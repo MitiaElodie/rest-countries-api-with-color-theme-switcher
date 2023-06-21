dc3bb60a458997f7412ea5f0d65d5a6082c92c29
Anjara Mitia Elodie <elodie.mitia@conformit.com>
2023-06-21 16:48:43 -0400

    HTML: add searchBar in CountryList

--- src/views/CountryList.vue
+++ src/views/CountryList.vue
@@ -1,7 +1,11 @@
 <script>
 import { mapState } from 'vuex'
+
+import SearchBar from '@/components/SearchBar.vue';
+
 export default {
    name: 'CountryList',
+   components: { SearchBar },
    computed: {
       ...mapState(['countryList'])
    }
@@ -10,6 +14,8 @@ export default {
 
 <template>
    <div class="country-list">
-      Country list
+      <div class="country-list__header">
+         <SearchBar class="country-list__search-bar"/>
+      </div>
    </div>
 </template>
