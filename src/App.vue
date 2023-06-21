ef99595987e6e7c189968966eaf494ec37472740
Anjara Mitia Elodie <elodie.mitia@conformit.com>
2023-06-21 12:00:06 -0400

    CSS: add the font-family

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <script>
 import CountryCard from '@/components/CountryCard.vue'
-
+import countryList from '@/data/data.json'
 export default {
   name: 'App',
   components: {
     CountryCard
+  },
+
+  data() {
+    return {
+      countryList: countryList
+    }
   }
 }
 </script>
 
 <template>
-  <CountryCard msg="Welcome to Your Vue.js App"/>
+  <CountryCard
+    :country="countryList[10]"
+  />
 </template>
 
 
-<style>
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+}
+
+:root {
+  font-family: 'Nunito Sans';
+}
 </style>
