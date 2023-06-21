573436d7b0acf285a9852cc8d420127f29941dfd
Anjara Mitia Elodie <elodie.mitia@conformit.com>
2023-06-21 16:48:14 -0400

    HTML + JS + CSS: create the component searchBar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+   name: 'SearchBar',
+   data() {
+      return {
+         searchValue: ''
+      }
+   }
+}
+</script>
+
+<template>
+   <div class="search-bar">
+      <input
+         v-model="searchValue"
+         class="search-bar__input"
+         placeholder="Search for a country..."
+      />
+   </div>
+</template>
+
+<style lang="scss">
+.search-bar {
+   &__input {
+      border: none;
+   }
+}
+</style>
