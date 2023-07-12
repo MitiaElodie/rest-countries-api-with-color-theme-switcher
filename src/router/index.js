397926614774534c8d61bed6d476a1264aee921c
Anjara Mitia Elodie <elodie.mitia@conformit.com>
2023-07-12 11:27:30 -0400

    Correction webchunkName

--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,13 @@ const routes = [
   {
     path: '/',
     name: 'CountryList',
-    component: () => import(/* webpackChunkName: 'CountryDetails' */'@/views/CountryList.vue'),
+    component: () => import(/* webpackChunkName: 'CountryList' */'@/views/CountryList.vue'),
   },
   {
    path: '/country/:countryId(\\d+)',
    name: 'CountryDetails',
    component: () => import(/* webpackChunkName: 'CountryDetails' */'@/views/CountryDetails.vue'),
- },
+  },
 ]
 
 const router = createRouter({
